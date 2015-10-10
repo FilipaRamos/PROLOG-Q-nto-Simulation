208c9d7403f4cc892ce5bb02819b110cef7084e4
IneSantos <inesalexcarneiro@hotmail.com>
2015-10-10 18:14:01 +0100

    novos ficheiros  ainda a trabalhar

---
+++ teste.pl
@@ -0,0 +1,15 @@
+cor(r). 
+cor(b).
+cor(g).
+cor(y).
+
+forma(*).
+forma(!).
+forma(#).
+forma(+).
+forma(&).
+
+
+PlinhaHorizontal(N) :- N>0 , write(' '), write ("----"), N1 is N-1 , linhaHorizontal(N1).
+linhaVertical(N) :- N >0, write('|'), write("    ") , write('|'), N1 is N-1, linhaVertical(N1).
+SlinhaHorizontal(N) :-
