44ff82d99368c83156dfd8cd62b256c01cfc12e4
IneSantos <inesalexcarneiro@hotmail.com>
2015-10-11 17:49:14 +0100

    letters visible, missing numbers

--- 1st project.pl
+++ 1st project.pl
@@ -22,21 +22,24 @@ piece(C, F) :- color(C), shape(F).
 
 writepiece(piece(C,F)) :- write(' '), print(C), print(F), write(' ').
 
+fguideLine(N, CC) :- CC==N, write('    ').
+fguideLine(N, CC) :- CC < N, CC2 is CC+1, write('    '), L is 65+CC, format('~1c', [L]) , write(' '), fguideLine(N, CC2).
+
 fHorizontalLine(0).
-fHorizontalLine(N) :- N>0 , write(' '), write('----'), N1 is N-1 , fHorizontalLine(N1).
+fHorizontalLine(N) :- N>0 , write('  '), write('----'), N1 is N-1 , fHorizontalLine(N1).
 
-sHorizontalLine(0, []) :- write('|').
-sHorizontalLine(N, [L|R]) :- N>0, write('|'), writepiece(L), N1 is N-1, sHorizontalLine(N1, R).
+sHorizontalLine(0, [], _CC) :- write('|'),write(' '), write(_CC).
+sHorizontalLine(N, [L|R], _CC) :- N>0 , write(_CC), write(' '), write('|'), writepiece(L), N1 is N-1, sHorizontalLine(N1, R, _CC2).
 
-tHorizontalLine(0) :- write('|').
-tHorizontalLine(N) :- N>0, write('|'), write('----'), N1 is N-1 , tHorizontalLine(N1).
+tHorizontalLine(0) :-  write('|').
+tHorizontalLine(N) :- N>0, write(' '), write('|'), write('----'), N1 is N-1 , tHorizontalLine(N1).
 
 displayBoardaux([]).
-displayBoardaux([L1]) :- length(L1,N1), sHorizontalLine(N1,L1), nl.
-displayBoardaux([L1|R]) :- R \= [], length(L1,N1), sHorizontalLine(N1, L1), nl, tHorizontalLine(N1), nl, displayBoardaux(R).
+displayBoardaux([L1]) :- length(L1,N1), sHorizontalLine(N1,L1,0), nl.
+displayBoardaux([L1|R]) :- R \= [], length(L1,N1), sHorizontalLine(N1, L1,0), nl, tHorizontalLine(N1), nl, displayBoardaux(R).
 
-displayBoard([L1|R]) :-  length(L1,N1), fHorizontalLine(N1), nl,
-						displayBoardaux([L1|R]), fHorizontalLine(N1), nl.
+displayBoard([L1|R]) :-  length(L1,N1), nl, fguideLine(N1, 0), nl, fHorizontalLine(N1), nl,
+						displayBoardaux([L1|R]), fHorizontalLine(N1), nl, fguideLine(N1, 0), nl.
 
 
 createMatrix(W, H, Matrix) :- listElement(L,W,piece(' ',' ')), listElement(Matrix,H,L). 
