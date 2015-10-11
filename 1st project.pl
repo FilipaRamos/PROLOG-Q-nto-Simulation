02d7ca4ad91d20cda0607399b5e3888e681b7c64
FilipaRamos <fimamora@hotmail.com>
2015-10-11 22:39:22 +0100

    conflicts solved

--- 1st project.pl
+++ 1st project.pl
@@ -24,24 +24,28 @@ tile(C, F) :- color(C), shape(F).
 
 writetile(tile(C,F)) :- write(' '), print(C), print(F), write(' ').
 
+/*  A   B    C  D .... */
 fguideLine(N, CC) :- CC==N, write('    ').
 fguideLine(N, CC) :- CC < N, CC2 is CC+1, write('    '), L is 65+CC, format('~1c', [L]) , write(' '), fguideLine(N, CC2).
 
+/*   ---- ---- ----  */ 
 fHorizontalLine(0).
 fHorizontalLine(N) :- N>0 , write('  '), write('----'), N1 is N-1 , fHorizontalLine(N1).
 
-sHorizontalLine(0, [], _CC) :- write('|'),write(' '), write(_CC).
-sHorizontalLine(N, [L|R], _CC) :- N>0 , write(_CC), write(' '), write('|'), writetile(L), N1 is N-1, sHorizontalLine(N1, R, _CC2).
+/* |    |    |    |  */
+sHorizontalLine(0, []) :- write('|'),write(' ').
+sHorizontalLine(N, [L|R]) :- N>0, write(' '),  write('|'), writetile(L), N1 is N-1, sHorizontalLine(N1, R).
 
+/* |----|----|----| */ 
 tHorizontalLine(0) :-  write('|').
 tHorizontalLine(N) :- N>0, write(' '), write('|'), write('----'), N1 is N-1 , tHorizontalLine(N1).
 
-displayBoardaux([]).
-displayBoardaux([L1]) :- length(L1,N1), sHorizontalLine(N1,L1,0), nl.
-displayBoardaux([L1|R]) :- R \= [], length(L1,N1), sHorizontalLine(N1, L1,0), nl, tHorizontalLine(N1), nl, displayBoardaux(R).
+displayBoardaux([], _C).
+displayBoardaux([L1], C) :-  write(C) ,write(' '), length(L1,N1), sHorizontalLine(N1,L1), write(' '), write(C), nl.
+displayBoardaux([L1|R], C) :- R \= [], length(L1,N1), C < N1, C2 is C+1, write(C), write(' '), sHorizontalLine(N1, L1), write(' '), write(C), nl, write('  '), tHorizontalLine(N1), nl, displayBoardaux(R, C2).
 
-displayBoard([L1|R]) :-  length(L1,N1), nl, fguideLine(N1, 0), nl, fHorizontalLine(N1), nl,
-						displayBoardaux([L1|R]), fHorizontalLine(N1), nl, fguideLine(N1, 0), nl.
+displayBoard([L1|R]) :-  length(L1,N1), nl,write('  '), fguideLine(N1, 0), nl,write('  '), fHorizontalLine(N1), nl,
+						displayBoardaux([L1|R],1),  write('  '), fHorizontalLine(N1), nl, write('  '), fguideLine(N1, 0), nl.
 
 
 createMatrix(W, H, Matrix) :- listElement(L,W,tile(' ',' ')), listElement(Matrix,H,L). 
